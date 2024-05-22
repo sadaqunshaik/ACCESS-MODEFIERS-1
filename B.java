ec70e062be8b5e79c38cb8bf84bcb904ecb937dc
Your Name <sadaqunps@gmail.com>
2024-05-22 17:38:01 +0530

    tst

---
+++ B.java
@@ -0,0 +1,12 @@
+package pack3;
+import pack1.A;
+
+public class B{
+    public static void main(String[] args) {
+        
+     A a1=new A();
+     a1.m1();
+     System.out.println("Class m1 method - public");
+
+    }
+} 
---
+++ Test.java
@@ -0,0 +1,12 @@
+import pack1.A;
+package pack2;
+
+ class Test{
+    public static void main(String[] args) {
+        
+     A a1=new A();
+     a1.m1();
+      System.out.println("class and method is public");
+
+    }
+}
